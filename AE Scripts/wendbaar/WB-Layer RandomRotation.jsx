053dc6c186cd63cc003d2dcb1308803f8607082f
Leon Berenschot <leipeleon@gmail.com>
2016-11-26 13:20:51 +0100

    Add RandomScale and RandomRotation

---
+++ AE Scripts/wendbaar/WB-Layer RandomRotation.jsx
@@ -0,0 +1,12 @@
+﻿/*
+ * (C) Leon Berenschot Wendbaar.nl 2016
+ */
+
+{
+    var effectsGroup = app.project.activeItem.selectedLayers;
+
+    for (var i = 0; i < effectsGroup.length; i++){
+        var newRotation = Math.floor(generateRandomNumber() * 360);
+        effectsGroup[i].rotation.setValue(newRotation);
+    }
+}
---
+++ AE Scripts/wendbaar/WB-Layer RandomScale.jsx
@@ -0,0 +1,15 @@
+﻿/*
+ * (C) Leon Berenschot Wendbaar.nl 2016
+ */
+
+{
+    var effectsGroup = app.project.activeItem.selectedLayers;
+
+    for (var i = 0; i < effectsGroup.length; i++){
+        var newScale = Math.floor(generateRandomNumber() * 20 + 10);
+        var layerScale = effectsGroup[i].scale.value;
+        layerScale[0] = newScale;
+        layerScale[1] = newScale;
+        effectsGroup[i].scale.setValue(layerScale);
+    }
+}
