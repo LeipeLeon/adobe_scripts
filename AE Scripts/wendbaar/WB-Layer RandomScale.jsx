0cadf537db7b5f635c0ad0d65961557cb1a066e2
Leon Berenschot <leipeleon@gmail.com>
2018-06-28 21:53:12 +0200

    lineendings

--- AE Scripts/wendbaar/WB-Layer RandomScale.jsx
+++ AE Scripts/wendbaar/WB-Layer RandomScale.jsx
@@ -1,1 +1,18 @@
-﻿/* * (C) Leon Berenschot Wendbaar.nl 2016 */{    var effectsGroup = app.project.activeItem.selectedLayers;    var currentTime= app.project.activeItem.time;    for (var i = 0; i < effectsGroup.length; i++){        var newScale = Math.floor(generateRandomNumber() * 100 + 10);        var layerScale = effectsGroup[i].scale.value;        layerScale[0] = newScale;        layerScale[1] = newScale;//        effectsGroup[i].scale.setValue(layerScale);        effectsGroup[i].scale.setValueAtTime(currentTime, layerScale);    }}
+﻿
+/*
+ * (C) Leon Berenschot Wendbaar.nl 2016
+ */
+
+{
+  var effectsGroup = app.project.activeItem.selectedLayers;
+  var currentTime = app.project.activeItem.time;
+
+  for (var i = 0; i < effectsGroup.length; i++) {
+    var newScale = Math.floor(generateRandomNumber() * 100 + 10);
+    var layerScale = effectsGroup[i].scale.value;
+    layerScale[0] = newScale;
+    layerScale[1] = newScale;
+    //        effectsGroup[i].scale.setValue(layerScale);
+    effectsGroup[i].scale.setValueAtTime(currentTime, layerScale);
+  }
+}
