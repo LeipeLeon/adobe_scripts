00e0dc4f76bdff5eb0367137d6a3d047e690ccd3
Leon Berenschot <leipeleon@gmail.com>
2018-06-28 21:53:22 +0200

    Distribute evenly

---
+++ AE Scripts/wendbaar/WB-Layer StartPoint -> Distribute.jsx
@@ -0,0 +1,11 @@
+﻿/*
+ * (C) Leon Berenschot Wendbaar.nl 2014
+ */
+{
+    var previousEndTime = 0;
+    var effectsGroup = app.project.activeItem.selectedLayers;
+    for (var i = 0; i < effectsGroup.length; i++){
+        effectsGroup[i].startTime = previousEndTime;
+        previousEndTime = effectsGroup[i].outPoint;
+    }
+}
